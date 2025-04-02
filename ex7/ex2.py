fb4a1cc9e028a7aed8e63c4b601aaf1d4a3e0ee2
Damian <130228499+Dami-Zuk@users.noreply.github.com>
2025-04-02 21:34:46 +0300

    Developed exercise 2

--- ex7/ex2.py
+++ ex7/ex2.py
@@ -32,6 +32,7 @@ heavier mammal you get as your pet. Remember to implement the selection logic in
 main (e.g. roll_dice, get_side etc.)"""
 import random
 
+# Part 1 - Dice and GameOfDices classes
 class Dice:
     def __init__(self):
         self._prev_rolls = []
@@ -43,5 +44,103 @@ class Dice:
         value = random.randint(1,6)
         self._prev_rolls.append(value)
         return value
+
+
+class GameOfDices:
+    def __init__(self, dices: list):
+        self.dices = dices
+
+    def play(self):
+        print("Rolling all dices \n")
+        for i in len(players):
+            players[i] 
+        
+
+# Part 4 - Mammal class
+class Mammal:
+    def __init__(self, id: int, species: str, name: str, size: str, weight: int):
+        self.__id = id
+        self.__species = species
+        self.__name = name
+        self.__size = size
+        self.__weight = weight
+
+    # Part 5
+    def __str__(self):
+        return f"Name: {self.__name}, Species: {self.__species}, Size: {self.__size}, Weight: {self.__weight}kg"
+
+
+# Part 3 - Player class
+class Player:
+    def __init__(self, name: str, player_id: int):
+        self.__name = name
+        self.__player_id = player_id
+        self.__pet = None   # Part 5 - assigning a pet
+
+    @property
+    def name(self):
+        return self.__name
     
+    @property
+    def player_id(self):
+        return self.__player_id
     
+    @name.setter
+    def name(self, value):
+        self.__name = value
+
+    @player_id.setter
+    def player_id(self, value):
+        self.__player_id = value
+
+    # Part 5 - assigning a pet to the player and str method
+    def assign_pet(self, pet: Mammal):
+        if isinstance(pet, Mammal):     # isinstance(obj, type) returns True or False
+            self.__pet = pet
+        else:
+            raise ValueError("The pet must be of Mammal type!")
+
+    def __str__(self):
+        if self.__pet:
+            pet_info = str(self.__pet)
+        else:
+            pet_info = "No pet assigned."
+
+        return f"Player {self.__name} - ID: {self.__player_id} - Pet: {pet_info}"
+
+
+# DICES
+dice1 = Dice()
+dice2 = Dice()
+dice3 = Dice()
+list_of_dices = [dice1, dice2, dice3]
+
+# PLAYERS
+player1 = Player("Dobby", 123)
+player2 = Player("Johnny", 456)
+player3 = Player("Bingus", 789)
+
+players = {
+    player1.player_id: dice1,
+    player2.player_id: dice2,
+    player3.player_id: dice3,
+}
+
+# PETS
+monke = Mammal(1, "Gorilla", "Harambe", "BIG", 120)
+doggo = Mammal(2, "Shiba Inu", "Good Boi", "Medium", 20)
+catto = Mammal(3, "Abyssinian", "Hiss", "Small", 5)
+
+# TESTS
+# list_of_dices[1].roll()
+# print(f"{player1.name} rolled: {players[player1.player_id].roll()}")
+# print(f"{player2.name} rolled: {players[player2.player_id].roll()}")
+
+player1.assign_pet(monke)
+player2.assign_pet(doggo)
+player3.assign_pet(catto)
+
+print(player1)
+
+
+
